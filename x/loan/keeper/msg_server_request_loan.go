747f72d7db4692d8fbd8b137d8e71b680e4e4cdd
black <black02430horse@gmail.com>
2023-11-02 05:13:46 -0500

    feat: request-loan

--- x/loan/keeper/msg_server_request_loan.go
+++ x/loan/keeper/msg_server_request_loan.go
@@ -12,6 +12,30 @@ func (k msgServer) RequestLoan(goCtx context.Context, msg *types.MsgRequestLoan)
 
 	// TODO: Handling the message
 	_ = ctx
+	var loan = types.Loan {
+		Amount: msg.Amount,
+		Fee: msg.Fee,
+		Collateral: msg.Collateral,
+		Deadline: msg.Deadline,
+		State: "requested",
+		Borrower: msg.Creator,
+	}
+	borrower, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+        panic(err)
+    }
+
+	collateral, err := sdk.ParseCoinsNormalized(loan.Collateral)
+	if err != nil {
+        panic(err)
+    }
+
+	sdkError := k.bankKeeper.SendCoinsFromAccountToModule(ctx, borrower, types.ModuleName, collateral)
+	if sdkError != nil {
+        return nil, sdkError
+    }
+
+	k.AppendLoan(ctx, loan);
 
 	return &types.MsgRequestLoanResponse{}, nil
 }
--- x/loan/types/message_request_loan.go
+++ x/loan/types/message_request_loan.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"strconv"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
@@ -45,5 +46,30 @@ func (msg *MsgRequestLoan) ValidateBasic() error {
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
 	}
+	amount, _ := sdk.ParseCoinsNormalized(msg.Amount)
+    if !amount.IsValid() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "amount is not a valid Coins object")
+    }
+    if amount.Empty() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "amount is empty")
+    }
+    fee, _ := sdk.ParseCoinsNormalized(msg.Fee)
+    if !fee.IsValid() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "fee is not a valid Coins object")
+    }
+    deadline, err := strconv.ParseInt(msg.Deadline, 10, 64)
+    if err != nil {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "deadline is not an integer")
+    }
+    if deadline <= 0 {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "deadline should be a positive integer")
+    }
+    collateral, _ := sdk.ParseCoinsNormalized(msg.Collateral)
+    if !collateral.IsValid() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "collateral is not a valid Coins object")
+    }
+    if collateral.Empty() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "collateral is empty")
+    }
 	return nil
 }
