71c0bd07bcdf2262aaef1dcc4c32dd6b55767b3e
black <black02430horse@gmail.com>
2023-11-02 06:26:50 -0500

    feat: repay-loan

--- x/loan/keeper/msg_server_repay_loan.go
+++ x/loan/keeper/msg_server_repay_loan.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"loan/x/loan/types"
 )
 
@@ -12,6 +13,36 @@ func (k msgServer) RepayLoan(goCtx context.Context, msg *types.MsgRepayLoan) (*t
 
 	// TODO: Handling the message
 	_ = ctx
+
+	loan, found := k.GetLoan(ctx, msg.Id)
+	if !found {
+        return nil, sdkerrors.Wrapf(sdkerrors.ErrKeyNotFound, "key %d doesn't exist", msg.Id)
+    }
+    if loan.State != "approved" {
+        return nil, sdkerrors.Wrapf(types.ErrWrongLoanState, "%v", loan.State)
+    }
+
+	lender, _ := sdk.AccAddressFromBech32(loan.Lender)
+	borrower, _ := sdk.AccAddressFromBech32(loan.Borrower)
+	if msg.Creator != loan.Borrower {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Cannot repay: not the borrower")
+    }
+
+	amount, _ := sdk.ParseCoinsNormalized(loan.Amount)
+	fee, _ := sdk.ParseCoinsNormalized(loan.Fee)
+	//repay to lender
+	err := k.bankKeeper.SendCoins(ctx, borrower, lender, amount)
+	if err != nil {
+        return nil, err
+    }
+	//pay fee to lender
+	err = k.bankKeeper.SendCoins(ctx, borrower, lender, fee)
+	if err != nil {
+        return nil, err
+    }
+
+	loan.State = "repayed"
+	k.SetLoan(ctx, loan)
 
 	return &types.MsgRepayLoanResponse{}, nil
 }
